f6496f48933fc39c68b363f6dbf104b94794f4c9
kunal-singh-tamu <kunal_singh@tamu.edu>
2024-09-15 23:49:14 -0500

    adding 3 new movies to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,14 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-Jul-2010'},
+  {:title => 'The Dark Knight', :rating => 'PG-13',
+    :release_date => '18-Jul-2008'},
+  {:title => 'The Social Network', :rating => 'PG-13',
+    :release_date => '01-OCt-2010'}
+
 ]
 
 more_movies.each do |movie|
